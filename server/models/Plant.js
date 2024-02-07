963a5e9ea72fec2a4df088642f45271f5d123eb1
BaharOssareh <78626041+BaharOssareh@users.noreply.github.com>
2024-02-07 13:20:51 -0500

    folder structure and files added for server and client and initial setup

---
+++ server/models/Plant.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const plantSchema = new Schema({
+  plantName: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  description: {
+    type: String
+  },
+  image: {
+    type: String
+  },
+  careLevel: {
+    type: String,
+    required: true,
+  },
+  waterLevel: {
+    type: String,
+    required: true,
+  },
+  lightLevel: {
+    type: String,
+    required: true,
+  },
+  size: {
+    type: String,
+    required: true,
+  },
+  trait: {
+    type: String,
+    required: true,
+  },
+});
+
+const Plant = mongoose.model('Plant', plantSchema);
+
+module.exports = Plant;
